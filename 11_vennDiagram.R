b6a522dfcf9cc93a170ce6b060694c26adb6e2da
Umar Niazi <uhkniazi@gmail.com>
2021-03-12 10:53:05 +0000

    added some changes to volcano plots to make it more fancy

--- 11_vennDiagram.R
+++ 11_vennDiagram.R
@@ -32,26 +32,35 @@ f_plotVolcano = function(dfGenes, main, p.adj.cut = 0.1, fc.lim = c(-3, 3)){
   y.cut = -1 * log10(0.01)
   col = rep('lightgrey', times=length(p.val))
   c = which(dfGenes$adj.P.Val < p.adj.cut)
-  col[c] = 'red'
-  plot(fc, p.val, pch=20, xlab='Fold Change', ylab='-log10 P.Value', col=col, main=main, xlim=fc.lim, cex=0.5)
+  col[c] = 'pink4'
+  col[which(dfGenes$adj.P.Val < p.adj.cut & (dfGenes$logFC) < 0)] = 'skyblue'
+  col[which(dfGenes$adj.P.Val < p.adj.cut & (dfGenes$logFC) > 1.5)] = 'red'
+  col[which(dfGenes$adj.P.Val < p.adj.cut & (dfGenes$logFC) < -1.5)] = 'royalblue4'
+  plot(fc, p.val, pch=20, xlab='log Fold Change', ylab='-log10 P.Value', col=col, main=main, xlim=fc.lim, cex=0.6)
   abline(v = 0, col='grey', lty=2)
-  abline(h = y.cut, col='red', lty=2)
+  #abline(h = y.cut, col='red', lty=2)
   # second cutoff for adjusted p-values
-  y.cut = quantile(p.val[c], probs=0.97)
+  y.cut = quantile(p.val[c], probs=0.99)
   #abline(h = y.cut, col='red')
   # identify these genes
   g = which(p.val > y.cut)
+  g = c(g, which(dfGenes$logFC < -1.5 & dfGenes$adj.P.Val < p.adj.cut))
+  g = c(g, which(dfGenes$logFC > 2 & dfGenes$adj.P.Val < p.adj.cut))
   lab = dfGenes[g, 'SYMBOL']
-  text(dfGenes$logFC[g], y = p.val[g], labels = lab, pos=2, cex=0.6)
+  text(dfGenes$logFC[g], y = p.val[g], labels = lab, pos=2, cex=0.8)
 }
 
 
 for (i in seq_along(cvTitle)){
-  df = ldfData[[i]]
-  hist(df$logFC, xlab='Log Fold Change', ylab='', main=paste('Fold Changes ', cvTitle[i], sep=''))
-  f_plotVolcano(df, cvTitle[i], 0.01, fc.lim=c(-2, 3))
+  df = ldfData[[1]]
+  hist(df$logFC, xlab='Log Fold Change', ylab='', main=paste('SepT0 vs CarT0',sep=''))
+  f_plotVolcano(df, main = 'SepT0 vs CarT0', 0.01, fc.lim=c(-3, 3))
 }
 
+legend('topleft', legend = c('FDR=1% & logFC > 0', 
+                             'FDR=1% & logFC > 1.5',
+                             'FDR=1% & logFC < 0',
+                             'FDR=1% & logFC < -1.5'), col = c('pink4', 'red', 'skyblue', 'royalblue4'), pch=20, bty='n', cex=1.2)
 names(ldfData)
 ## select significant genes
 round(quantile(abs(ldfData[[1]]$logFC), 0:10/10), 3)
