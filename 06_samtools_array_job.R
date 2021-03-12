0bc02a43ec1a609d85e7f0f3d7b432ff9ab45aee
Umar Niazi <uhkniazi@gmail.com>
2021-03-12 15:15:34 +0000

    samtools array job completed

--- 06_samtools_array_job.R
+++ 06_samtools_array_job.R
@@ -1,7 +1,7 @@
 # File: 06_samtools_array_job.R
 # Auth: umar.niazi@kcl.as.uk
 # DESC: create a parameter file and shell script to run array job on hpc
-# Date: 8/2/2021
+# Date: 12/3/2021
 
 
 ## set variables and source libraries
@@ -17,7 +17,7 @@ dbListTables(db)
 # check how many files each sample has
 g_did
 q = paste0('select count(File.idSample) as files, Sample.idData, Sample.title, Sample.id as SampleID, Sample.group1 as g1 from File, Sample
-           where (Sample.idData = 50 and File.idSample = Sample.id) group by File.idSample')
+           where (Sample.idData = 51 and File.idSample = Sample.id) group by File.idSample')
 dfQuery = dbGetQuery(db, q)
 dfQuery$title = gsub(" ", "", dfQuery$title, fixed = T)
 head(dfQuery)
@@ -25,7 +25,7 @@ dfQuery = na.omit(dfQuery)
 
 # get the count of files
 q = paste0('select File.*, Sample.idData, Sample.group1 as g1 from File, Sample 
-           where (Sample.idData = 50) and (File.idSample = Sample.id) and (File.type = "fastq")')
+           where (Sample.idData = 51) and (File.idSample = Sample.id) and (File.type = "fastq")')
 dfCounts = dbGetQuery(db, q)
 head(dfCounts)
 i = is.na(dfCounts$g1)
@@ -36,7 +36,7 @@ dim(dfCounts)
 
 # for each sample id, get the corresponding files
 cvQueries = paste0('select File.*, Sample.title from File, Sample 
-                   where (Sample.idData = 50 and Sample.id =', dfQuery$SampleID, ') and (File.idSample = Sample.id) and 
+                   where (Sample.idData = 51 and Sample.id =', dfQuery$SampleID, ') and (File.idSample = Sample.id) and 
                    (File.type = "fastq")')
 
 # set header variables 
@@ -58,7 +58,7 @@ cvMail = '#SBATCH --mail-type=END,FAIL'
 # set array job loop
 length(cvQueries)
 dim(dfCounts)/2
-cvArrayJob = '#SBATCH --array=1-56'
+cvArrayJob = '#SBATCH --array=1-33'
 
 # set the directory names
 cvInput = 'input/'
