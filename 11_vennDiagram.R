d918792c157ccf92aab54902183900919afeeba0
Umar Niazi <uhkniazi@gmail.com>
2021-03-19 11:35:04 +0000

    enhanced volcano plots done for the 3 comparisons with significant results

--- 11_vennDiagram.R
+++ 11_vennDiagram.R
@@ -1,28 +1,12 @@
 # File: 11_vennDiagram.R
 # Auth: umar.niazi@kcl.ac.uk
 # DESC: using the results for each contrast, create venn diagrams and some plots
-# Date: 10/2/2021
+# Date: 19/3/2021
 
 source('header.R')
 
-lFiles = list.files('dataID50/results/', pattern='DEAnalysis*', full.names = T, ignore.case = T)
-
-ldfData = lapply(lFiles, function(x) as.data.frame(read.csv(x, header=T, row.names=1, stringsAsFactors = F)))
-names(ldfData) = lFiles
-sapply(ldfData, nrow)
-
-# put everything in one order by row names
-rn = rownames(ldfData[[1]])
-head(rn)
-
-ldfData = lapply(ldfData, function(df){
-  df = df[rn,]
-})
-
-sapply(ldfData, function(df) identical(rownames(df), rn))
-
-cvTitle = gsub('dataID50/results//DEAnalysis_', '', names(ldfData))
-cvTitle = gsub('.xls', '', cvTitle)
+cvFile = 'dataID51/results/Tcells/DEAnalysis_SC_T0vsHC_T0.xls'
+dfData = read.csv(cvFile, header=T, stringsAsFactors = F, row.names=1)
 
 ## redefine plot volcano
 f_plotVolcano = function(dfGenes, main, p.adj.cut = 0.1, fc.lim = c(-3, 3)){
@@ -44,26 +28,20 @@ f_plotVolcano = function(dfGenes, main, p.adj.cut = 0.1, fc.lim = c(-3, 3)){
   #abline(h = y.cut, col='red')
   # identify these genes
   g = which(p.val > y.cut)
-  g = c(g, which(dfGenes$logFC < -1.5 & dfGenes$adj.P.Val < p.adj.cut))
-  g = c(g, which(dfGenes$logFC > 2 & dfGenes$adj.P.Val < p.adj.cut))
+  #g = which(dfGenes$adj.P.Val < p.adj.cut)
+  #g = c(g, which(dfGenes$logFC < -1.5 & dfGenes$adj.P.Val < p.adj.cut))
+  #g = c(g, which(dfGenes$logFC > 1.5 & dfGenes$adj.P.Val < p.adj.cut))
   lab = dfGenes[g, 'SYMBOL']
   text(dfGenes$logFC[g], y = p.val[g], labels = lab, pos=2, cex=0.8)
 }
 
+range(dfData$logFC)
+cvFile
+f_plotVolcano(dfData, main = 'T Cells: SCT0 vs HCT0', 0.01, fc.lim=c(-4, 8.5))
 
-for (i in seq_along(cvTitle)){
-  df = ldfData[[1]]
-  hist(df$logFC, xlab='Log Fold Change', ylab='', main=paste('SepT0 vs CarT0',sep=''))
-  f_plotVolcano(df, main = 'SepT0 vs CarT0', 0.01, fc.lim=c(-3, 3))
-}
+legend('topleft', legend = c('FDR=10% & logFC > 0', 
+                             'FDR=10% & logFC > 1.5',
+                             'FDR=10% & logFC < 0',
+                             'FDR=10% & logFC < -1.5'), col = c('pink4', 'red', 'skyblue', 'royalblue4'), pch=20, bty='n', cex=0.8)
 
-legend('topleft', legend = c('FDR=1% & logFC > 0', 
-                             'FDR=1% & logFC > 1.5',
-                             'FDR=1% & logFC < 0',
-                             'FDR=1% & logFC < -1.5'), col = c('pink4', 'red', 'skyblue', 'royalblue4'), pch=20, bty='n', cex=1.2)
-names(ldfData)
-## select significant genes
-round(quantile(abs(ldfData[[1]]$logFC), 0:10/10), 3)
-table(ldfData[[1]]$adj.P.Val < 0.01)
-dfContrast1.sub = ldfData[[1]][ldfData[[1]]$adj.P.Val < 0.01,]
 
